45a3b4aaa428f23b9e11930b696ca5f1b59cbbc9
Johnny West <83635638+Dmitriy-Shiborkin@users.noreply.github.com>
2022-11-24 12:27:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,85 +1,76 @@
+import telebot
+import requests
+import json
 
-def sort(array):
-    for i in range(len(array)):
-        idx_max = i
-        for j in range(i, len(array)):
-            if array[j] < array[idx_max]:
-                idx_max = j
-    return array
+TOKEN = '5931236300:AAFXSiQoduJ-h-BcaJI3Tf5J7KGs_Q0mgnA'
+keys = {'евро': 'EUR', 'доллар': 'USD', 'рубль': 'RUB'}
+
+class ConvertionException(Exception):
+    pass
 
 
-def binary_search(array, element, left, right):
-    if element < array[0]:
-        return False
-    if left > right:
-        return right
+class CryptoConverter:
+    @staticmethod
+    def convert(quote: str, base: str, amount: str):
 
-    middle = (right + left) // 2
-    if (array[middle] == element) or (array[middle-1] < element <= array[middle]):
-        return middle - 1
-    elif element < array[middle]:
-        return binary_search(array, element, left, middle - 1)
+        if quote == base:
+            raise ConvertionException(f'Невозможно перевести одинаковые валюты {base}.')
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise ConvertionException(f'Не удалось отработать валюту {quote}.')
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise ConvertionException(f'Не удалось отработать валюту {base}.')
+
+        try:
+            amount_ticker = float(amount)
+        except KeyError:
+            raise ConvertionException(f'Не удалось обработать количество {amount}.')
+
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
+        total_base = json.loads(r.content)[keys[base]] * amount_ticker
+        return total_base
+
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['start', 'help'])
+def help(message: telebot.types.Message):
+    text = 'Отправьте сообщение боту в виде:\n<имя валюты цену которой вы хотите узнать>' \
+           '<имя валюты в которой надо узнать цену первой валюты>' \
+           '<количество первой валюты>\nУвидеть список всех доступных валют: /values'
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = 'Доступные валюты:'
+    for key in keys.keys():
+        text = '\n'.join((text, key,))
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(content_types=['text', ])
+def convert(message: telebot.types.Message):
+    try:
+        values = message.text.split(' ')
+
+        if len(values) != 3:
+            raise ConvertionException('Слишком много параметров')
+
+        quote, base, amount = values
+        total_base = CryptoConverter.convert(quote, base, amount)
+    except ConvertionException as e:
+        bot.reply_to(message, f'ошибка пользователя\n{e}')
+    except Exception as e:
+        bot.reply_to(message, f'не удалось обработать команду\n{e}')
     else:
-        return binary_search(array, element, middle + 1, right)
+        text = f'Цена {amount} {quote} в {base} - {total_base}'
+        bot.send_message(message.chat.id, text)
 
 
-array = list(map(int, input('Введите целые числа через пробел:\n').split()))
-print('Отсортированный список:  ', sort(array))
-print('Индексы элементов:   ', [i for i in range(len(array))])
-element = int(input('Введите любое число: '))
-result = binary_search(array, element, 0, len(array) - 1)
-
-if not result and isinstance(result, bool):
-    print("Введенное число минимальное")
-elif result == -1:
-    print("Введенное число совпадает с нулевым элементом списка")
-elif result == len(array) - 1:
-    print("Введенное число больше последнего (%d - го) элемента списка" % (len(array) - 1))
-else:
-    print("Номер позиции элемента, который меньше введенного числа:", result)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+bot.polling()
